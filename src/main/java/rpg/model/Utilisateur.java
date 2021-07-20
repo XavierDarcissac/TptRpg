f36ba123c08d7e5765ef97ea332495ace454da30
Alexandre Lacoste <alex.lacoste64@orange.fr>
2021-07-20 15:05:53 +0200

    utilisateur vide

--- src/main/java/rpg/model/Utilisateur.java
+++ src/main/java/rpg/model/Utilisateur.java
@@ -12,66 +12,6 @@ import javax.persistence.Transient;
 @DiscriminatorValue("user")
 public class Utilisateur extends Compte {
 	
-	@Transient
-	//@OneToOne()
-	//@JoinColumn()
-	
-	private Hero personnage;
-	@Transient
-	//@OneToOne()
-	//@JoinColumn()
-	private Inventaire inventaire;
-	@Transient
-	//@OneToOne()
-	//@JoinColumn()
-	private Arme arme;
-	@Transient
-	//@OneToOne()
-	//@JoinColumn()
-	
-	private Armure armure;
-	@Transient
-	//@OneToOne()
-	//@JoinColumn()
-	
-	private Histoire histoire;
-	
-	@Column()
-	private double vie;
-	@Column()
-	private double attaque;
-	@Column()
-	private double defense;
-	@Column()
-	private double precision;
-	@Column()
-	private double vitesse;
-	@Column()
-	private double vieMax;
-	@Column()
-	private double attaqueMax;
-	@Column()
-	private double defenseMax;
-	@Column()
-	private double precisionMax;
-	@Column()
-	private double vitesseMax;
-	@Column()
-	private int cptEmpoisonnement;
-	@Column()
-	private int cptEtourdissement;
-	@Column()
-	private int cptSaignement;
-	@Column()
-	private int cptBrulure;
-	@Column()
-	private int cptCombat;
-	@Column()
-	private int cptCombatGagne;
-	@Column()
-	private int cptMonstreVaincu;
-	@Column()
-	private int exp;
 	
 	
 	
