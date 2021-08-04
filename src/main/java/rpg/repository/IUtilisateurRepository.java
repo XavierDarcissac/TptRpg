ac471bf1b9dab86f976d93baed6361a1f79fc8f4
Alexandre Lacoste <alex.lacoste64@orange.fr>
2021-08-04 10:54:32 +0200

    update utilisateur mail,mdp,pseudo + delete utilisateur by mail

--- src/main/java/rpg/repository/IUtilisateurRepository.java
+++ src/main/java/rpg/repository/IUtilisateurRepository.java
@@ -1,6 +1,7 @@
 package rpg.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -10,6 +11,26 @@ import rpg.model.Utilisateur;
 public interface IUtilisateurRepository extends JpaRepository<Utilisateur, Long> {
 	@Query("select u from Utilisateur u where u.pseudo = :pseudo")
 	Utilisateur findByPseudo(@Param("pseudo") String pseudo); // @Query
+	
+	@Modifying
+	@Query("delete Utilisateur u where u.mail = :mail")
+	void deleteUserByMail(@Param("mail") String mail);
+	
+	@Modifying
+	@Query("delete Utilisateur u where u.pseudo = :pseudo")
+	void deleteUserByPseudo(@Param("pseudo") String pseudo);
+	
+	@Modifying
+	@Query("update Utilisateur u set u.pseudo = ?1 where u.pseudo = ?2")	
+	void updateUserPseudo(String newPseudo,String pseudo);
+	
+	@Modifying
+	@Query("update Utilisateur u set u.mdp = ?1 where u.mail = ?2")
+	void updateUserMdpByUserMail(String newMdp, String mail);
+	
+	@Modifying
+	@Query("update Utilisateur u set u.mdp = ?1, u.mail = ?2 where u.mail = ?3")
+	void updateUserMdpAndMailByUserMail(String newMdp, String newMail, String mail);
 	
 
 }
